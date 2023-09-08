e36992d3e1fc4fb49c5d1409478a1565df2cc17d
VladimirSavchukIThub <113594484+VladimirSavchukIThub@users.noreply.github.com>
2023-09-08 08:59:22 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,15 @@
 import ru.proglib.Animal;
-
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        var myAn = new Animal(15,4, 2);
+        Scanner myObj = new Scanner(System.in);
+        System.out.println("Enter speed");
+        int a = myObj.nextInt();
+        System.out.println("Enter number of paws");
+        int b = myObj.nextInt();
+        System.out.println("Enter random number(voice)");
+        int c = myObj.nextInt();
+        var myAn = new Animal(a,b, c);
         myAn.start();
         myAn.stop();
         System.out.print("Кол-во лап:");
